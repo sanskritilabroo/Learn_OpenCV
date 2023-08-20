e74d375d756efe1706959f6f88bccbf5b3b6e73b
sanskritilabroo <86831838+sanskritilabroo@users.noreply.github.com>
2023-08-21 02:27:59 +0530

    Update 22.ShapeDetection.py

--- basics/22.ShapeDetection.py
+++ basics/22.ShapeDetection.py
@@ -1,15 +1,37 @@
 import numpy as np
 import cv2
 
-img=cv2.imread("smarties.png")
-output=img.copy()
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-gray=cv2.medianBlur(gray,5)
-circles=cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
 
-detected_circles=np.uint16(np.around(circles))
-for (x,y,r) in detected_circles[0,:]:
-    cv2.circle(output,(x,y),r, (0,255,0),3)
-cv2.imshow("shapes",output)
+
+img = cv2.imread('shapes.jpg')
+imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
+contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+cv2.imshow("img", img)
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+    cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1] - 5
+    if len(approx) == 3: #3 sided triangle
+        cv2.putText(img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    elif len(approx) == 4: #square or rectangle
+        x1 ,y1, w, h = cv2.boundingRect(approx)
+        aspectRatio = float(w)/h
+        print(aspectRatio)
+        if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+          cv2.putText(img, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0))
+        else:
+          cv2.putText(img, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    elif len(approx) == 5: #5 sided pentagon
+        cv2.putText(img, "Pentagon", (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0))
+    elif len(approx) == 10: #10 sided star
+        cv2.putText(img, "Star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+    else:
+        cv2.putText(img, "Circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+
+
+cv2.imshow("shapes", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
